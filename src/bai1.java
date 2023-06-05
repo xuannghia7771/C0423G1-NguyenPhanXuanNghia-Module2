50c01a3751070ee9e061db2d9048562e54c993c0
xuannghia <nghiaphan33dn@gmail.com>
2023-06-05 13:59:18 +0700

    first commit

---
+++ src/bai1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class bai1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhập tên: ");
+        String name = sc.nextLine();
+        System.out.printf("Hello "+name);
+
+    }
+}
