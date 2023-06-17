4719842f96a42915aa9f105b4503d4c407bbf193
xuannghia <nghiaphan33dn@gmail.com>
2023-06-17 11:28:21 +0700

    bai tap ngay 16-06

--- src/ss11_stack_and_queue/exercise/kiem_tra_ngoac/CheckBracket.java
+++ src/ss11_stack_and_queue/exercise/kiem_tra_ngoac/CheckBracket.java
@@ -3,18 +3,39 @@ package ss11_stack_and_queue.exercise.kiem_tra_ngoac;
 import java.util.Stack;
 
 public class CheckBracket {
-    public static void main(String[] args) {
-        String str = "s * (s – a) * (s – b) * (s – c)";
-        Stack<Character> stack = new Stack<>();
+    public static void checkBracket(String str) {
+        Stack<Character> openBracketStack = new Stack<>();
+        Stack<Character> closeBracketStack = new Stack<>();
         for (int i = 0; i < str.length(); i++) {
-            char c = str.charAt(i);
-            if (c == '(' || c == ')') {
-                stack.push(c);
+            if (String.valueOf(str.charAt(i)).equals("(")) {
+                openBracketStack.push(str.charAt(i));
+            }
+            if (String.valueOf(str.charAt(i)).equals(")")) {
+                closeBracketStack.push(str.charAt(i));
             }
         }
-        if (stack.size() % 2 == 0) {
-            System.out.println("true");
-        } else
-            System.out.println("false");
+        while (!openBracketStack.isEmpty() && !closeBracketStack.empty()) {
+            openBracketStack.pop();
+            closeBracketStack.pop();
+        }
+        if (openBracketStack.isEmpty() && closeBracketStack.isEmpty()) {
+            System.out.println("well");
+        } else {
+            System.out.println("???");
+        }
+    }
+
+    public static void main(String[] args) {
+        String str1 = "s * (s – a) * (s – b) * (s – c)";
+        String str2 = "(– b + (b2 – 4*a*c)^0.5) / 2*a";
+        String str3 = "s * (s – a) * (s – b * (s – c)";
+        String str4 = "s * (s – a) * s – b) * (s – c)";
+
+        checkBracket(str1);
+        checkBracket(str2);
+        checkBracket(str3);
+        checkBracket(str4);
+
+
     }
 }
